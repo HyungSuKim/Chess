66105e7ced3ec380a01dd32bfc2327e3eac46990
김형수 <@DESKTOP-ATK5IMT>
2017-11-10 16:21:03 +0900

    Rook class 수정 - 상수 MOVEABLE -> MOVEABLE_SQUARE 수정 및 주석 추가

--- Chess/src/com/bfpaul/chess/chessman/rook/Rook.java
+++ Chess/src/com/bfpaul/chess/chessman/rook/Rook.java
@@ -5,15 +5,16 @@ import javax.swing.BorderFactory;
 import com.bfpaul.chess.Images;
 import com.mommoo.flat.image.FlatImagePanel;
 import com.mommoo.flat.image.ImageOption;
-
+//chessman 패키지 내에서 Rook을 ImagePanel로써 생성하고 룩의 고유 속성을 가지고있는 클래스
 @SuppressWarnings("serial")
 public class Rook extends FlatImagePanel {
-	
-	final int MOVEABLE = 7;
-	
+//	룩은 어떻게 움직여도(상하좌우) 최대 7칸을 움직일수 있다는 생각에서 선언된 상수
+	final int MOVEABLE_SQUARE = 7;
+//	말이 흰색인지 아닌지를 나타내주는 변수
 	private boolean isWhite;
+//	rook의 캐슬링(룩 킹 둘다 움직인 적이 없어야됨)을 구현하기 위해 가지고 있어야 될 속성이라 판단되어 선언한 변수(기본이 false)
 	private boolean isMoved;
-	
+//	룩의 생성자인데 흰색이면(true) 검정잭이면 (false)를 받아서 이를 이용해서 흰색 또는 검정색 룩이미지를 가져와서 적용시켜준다.
 	public Rook(boolean isWhite) {
 		
 		this.isWhite = isWhite;
@@ -26,19 +27,19 @@ public class Rook extends FlatImagePanel {
 		}
 		setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
 	}
-	
+//	말이 흰색인지 아닌지 확인하기 위해서 isWhite를 반환받아서 확인한다.
 	public boolean getIsWhite() {
 		return isWhite;
 	}
-	
-	public int getMoveable() {
-		return MOVEABLE;
+//	움직일수 있는 칸의 수를 가져와주는 get메서드
+	public int getMoveableSquare() {
+		return MOVEABLE_SQUARE;
 	}
-	
+//	움직임이 발생했을때 움직였음을 표시하기위한 메서드
 	public void setIsMoved() {
 		isMoved = true;
 	}
-	
+//	움직였는지 아닌지 반환하는 메서드
 	public boolean getIsMoved() {
 		return isMoved;
 	}
