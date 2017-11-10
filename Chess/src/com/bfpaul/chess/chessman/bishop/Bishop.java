c922813c22707962ce1b4f4be280c3cdb8600547
김형수 <@DESKTOP-ATK5IMT>
2017-11-10 14:59:45 +0900

    chessman - bishop 상수 MOVEABLE -> MOVEABLE_SQUARE로 수정 및 주석 추가

--- Chess/src/com/bfpaul/chess/chessman/bishop/Bishop.java
+++ Chess/src/com/bfpaul/chess/chessman/bishop/Bishop.java
@@ -5,14 +5,16 @@ import javax.swing.BorderFactory;
 import com.bfpaul.chess.Images;
 import com.mommoo.flat.image.FlatImagePanel;
 import com.mommoo.flat.image.ImageOption;
-
+//chessman(체스말) 패키지에 포함된 bishop을 해주고 그 bishop의 속성을 가지고있는 클래스이다.
 @SuppressWarnings("serial")
 public class Bishop extends FlatImagePanel {
-	
-	private final int MOVEABLE = 7;
-	
+//	비숍이 이동가능한 square의 갯수를 나타낸 상수이다. 비숍이 대각선으로 이동하는지 아닌지는
+//	비숍 자체가 알아야 될 필요는 없다고 판단하였다. 단 어찌되었든 최대 7칸은 무조건 움직일 수 있다는 생각에서 작성한 변수이다.
+	private final int MOVEABLE_SQUARE = 7;
+//	비숍의 속성이 흰색말인지 아닌지를 가지고 있기위해서 작성한 멤버변수인데 추후 같은편의 말을 파악할때나 상대편의 말을 공격할때 쓰기위해서 작성했다. 
 	private boolean isWhite;
 	
+//	비숍의 생성자인데 흰색이면(true) 검정잭이면 (false)를 받아서 이를 이용해서 흰색 또는 검정색 비숍이미지를 가져와서 적용시켜준다.
 	public Bishop(boolean isWhite) {
 		
 		this.isWhite = isWhite;
@@ -26,10 +28,12 @@ public class Bishop extends FlatImagePanel {
 		setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
 	}
 	
-	public int getMoveable() {
-		return MOVEABLE;
+//	움직일 수 있는 칸 수를 가져오는 메서드이다.
+	public int getMoveableSquare() {
+		return MOVEABLE_SQUARE;
 	}
 	
+//	흰색인지 아닌지를 가져오는 메서드이다.
 	public boolean getIsWhite() {
 		return isWhite;
 	}
