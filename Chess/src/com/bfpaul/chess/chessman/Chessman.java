9f70a2939368125b5417f550bb900bc131053fec
김형수 <@DESKTOP-ATK5IMT>
2017-11-12 16:20:59 +0900

    주석 수정 및 변수이름 수정을 통한 Chessman class의 역할 재정의
    Chessman(체스말) class는 어떠한 역할을 하는지 어떠한 정보를 가지고있어야 하는지 재고하여 주석으로 표현하고 기존의
    변수이름을 변경하여 가지고 있는 정보의 의미를 보다 명확하게 표현하였다.

--- Chess/src/com/bfpaul/chess/chessman/Chessman.java
+++ Chess/src/com/bfpaul/chess/chessman/Chessman.java
@@ -4,34 +4,34 @@ import javax.swing.BorderFactory;
 
 import com.mommoo.flat.image.FlatImagePanel;
 import com.mommoo.flat.image.ImageOption;
-//chessman(체스말) 패키지에 포함된 하위 클래스(King, Queen, Bishop, Knight, Rook, Pawn)의 인스턴스 생성 시
-//체스말의 속성 중 흰색인지 아닌지(색상) 정보를 가지고 있는 부모 클래스이다.
-//이 부모클래스는 이미지 패널을 상속받도록 구현되었는데 이유는 체스말을 생성했을때 하나의 이미지패널로써 역할하도록 하기 위함이다.
+
+//체스말의 속성을 가지고 있다.
+//흰색인지 아닌지(색상) 그리고 체스말이 움직일 수 있는 칸 수를 가지고 있으며 체스말의 이미지 중 하나로 셋팅된다.
 @SuppressWarnings("serial")
 public class Chessman extends FlatImagePanel {
-//	자손 클래스의 인스턴스 생성시 흰색인지 아닌지(색상) 정보를 받아서 저장하기 위한 멤버변수이다.	
+//	체스말이 흰색인지 아닌지 = 체스말은 흰색 아니면 검정색	
 	private boolean isWhite;
-//	체스말의 이동가능한 square의 갯수를 나타낸 상수이다. 자손의 인스턴스가 생성될 때 입력된 값으로 초기화해준다.
-	private final int MOVEABLE_SQUARE;
+//	체스말의 이동가능한 square의 갯수
+	private final int MOVEABLE_SQUARE_COUNT;
 	
-//	자손 클래스의 인스턴스 생성시 흰색인지 아닌지(색상) 정보를 받아서 멤버변수에 저장한다.
-//	그리고 자손클래스에서 각 자손의 움직일수 있는 칸수를 받아 멤버변수인 상수 MOVEABLE_SQUARE을 초기화해준다.
-//	또한 파라미터로써 체스맨타입을 받아서 이미지를 가져와 이미지를 이미지패널에 셋팅해준다.
-	protected Chessman(boolean isWhite, int moveableSquare, ChessmanType type) {
+//	체스말을 생성한다.
+//	체스말은 흰색인지 아닌지, 이동가능한 square의 갯수에 대한 정보를 갖고 
+//	흰색인지 아닌지 그리고 체스맨타입 정보를 이용해서 그에 맞는 이미지를 갖는다.
+	protected Chessman(boolean isWhite, int moveableSquareCount, ChessmanType chessmanType) {
 		this.isWhite = isWhite;
-		MOVEABLE_SQUARE = moveableSquare;
+		MOVEABLE_SQUARE_COUNT = moveableSquareCount;
 		
-		setImage(ChessmanImage.getChessmanImage(isWhite, type), ImageOption.MATCH_PARENT);
+		setImage(ChessmanImage.getChessmanImage(isWhite, chessmanType), ImageOption.MATCH_PARENT);
 		setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
 	}
 	
-//	흰색인지 아닌지를 가져오는 메서드이다.
+//	체스말이 흰색인지 아닌지를 반환한다.
 	public boolean getIsWhite() {
 		return isWhite;
 	}
 
 //	체스말의 이동가능한 square의 갯수를 반환한다
-	public int getMoveableSquare() {
-		return MOVEABLE_SQUARE;
+	public int getMoveableSquareCount() {
+		return MOVEABLE_SQUARE_COUNT;
 	}
 }
