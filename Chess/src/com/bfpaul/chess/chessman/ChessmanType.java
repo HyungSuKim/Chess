5611adcc4b392400baa37e3d8fe7d8fc25b6f138
김형수 <@DESKTOP-ATK5IMT>
2017-11-12 18:46:17 +0900

    ChessmanType class 주석 추가

--- Chess/src/com/bfpaul/chess/chessman/ChessmanType.java
+++ Chess/src/com/bfpaul/chess/chessman/ChessmanType.java
@@ -1,5 +1,5 @@
 package com.bfpaul.chess.chessman;
-
+// Chessman(체스말)의 타입과 각 타입의 말의 수가 최대 몇인지 지정해준다.
 public enum ChessmanType{
 	KING(1),
 	QUEEN(1),
@@ -7,13 +7,13 @@ public enum ChessmanType{
 	KNIGHT(2),
 	ROOK(2),
 	PAWN(8);
-	
+//	각 말의 최대 수
 	private final int MAX_COUNT;
-	
+
 	private ChessmanType(int maxCount) {
 		this.MAX_COUNT = maxCount;
 	}
-	
+// 각 말의 최대 수를 반환한다.	
 	public int getMaxCount() {
 		return MAX_COUNT;
 	}
