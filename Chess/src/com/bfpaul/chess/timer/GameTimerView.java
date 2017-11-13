9158543a8b69f906508a0a5a3f24f19a32fb4c8c
김형수 <@DESKTOP-ATK5IMT>
2017-11-13 18:59:58 +0900

    GameTimerView class 주석 추가 및 수정
    멤버변수 TimerPanel 2개 삭제 - GameTimerView로써 TimerPanel의 정보를 굳이 변수로 계속 가지고 있을
    필요가 없다고 판단하였다.

--- Chess/src/com/bfpaul/chess/timer/GameTimerView.java
+++ Chess/src/com/bfpaul/chess/timer/GameTimerView.java
@@ -13,35 +13,33 @@ import com.mommoo.flat.layout.linear.constraints.LinearSpace;
 import com.mommoo.flat.text.label.FlatLabel;
 import com.mommoo.flat.text.textarea.alignment.FlatHorizontalAlignment;
 import com.mommoo.flat.text.textarea.alignment.FlatVerticalAlignment;
-
+// 게임 시작시 플레이어(흰색과 검정색)의 타이머를 보여주고 버튼을 이용해서 턴을 넘겨주고자 구현하였다.
 @SuppressWarnings("serial")
 public class GameTimerView extends FlatPanel {
-	
+//	턴 종료 버튼
 	private FlatButton phaseEndButton = createPhaseEndButton();
-	
-	private TimerPanel whiteTimer = new TimerPanel(true);
-	private TimerPanel blackTimer = new TimerPanel(false);
 	
 	public GameTimerView() {
 		setLayout(new LinearLayout(10));
 		setBackground(Theme.DARK_BLUE_COLOR);
 		setOpaque(true);
-		add(whiteTimer, createMatchParentConstraints(3));
-		add(blackTimer, createMatchParentConstraints(3));
+		
+		add(new TimerPanel(true), createMatchParentConstraints(3));
+		add(new TimerPanel(false), createMatchParentConstraints(3));
 		add(createPhaseEndButtonPanel(), createMatchParentConstraints(1));
 	}
-	
+//	정해진 무게만큼의 영역을 차지하고 부모의 영역에 영역을 맞춰주는 제약조건을 반환한다. 
 	private LinearConstraints createMatchParentConstraints(int weight) {
 		return new LinearConstraints().setWeight(weight).setLinearSpace(LinearSpace.MATCH_PARENT);
 	}
-	
+//	턴종료 버튼의 패널을 만들어서 버튼에 대한 설명과 버튼을 하나의 패널로 묶어주려고 구현하였다.
 	private FlatPanel createPhaseEndButtonPanel() {
 		FlatPanel phaseEndButtonPanel = new FlatPanel(new LinearLayout(Orientation.VERTICAL, 0));
 		phaseEndButtonPanel.add(createPhaseEndButtonInfo(), createMatchParentConstraints(8));
 		phaseEndButtonPanel.add(phaseEndButton, createMatchParentConstraints(15));
 		return phaseEndButtonPanel;
 	}
-	
+//	턴종료 버튼에 대한 설명을 라벨로써 만들어준다
 	private FlatLabel createPhaseEndButtonInfo() {
 		FlatLabel phaseEndButtonInfo = new FlatLabel("차례");
 		phaseEndButtonInfo.setOpaque(false);
@@ -50,16 +48,16 @@ public class GameTimerView extends FlatPanel {
 		phaseEndButtonInfo.setFont(Theme.BOLD_FONT_15PT);
 		return phaseEndButtonInfo;
 	}
-	
+//	턴종료 버튼을 만들어준다
 	private FlatButton createPhaseEndButton() {
 		FlatButton phaseEndButton = new FlatButton("종료");
 		phaseEndButton.setBackground(Theme.YELLOW_COLOR);
 		phaseEndButton.setOnClickListener(new OnClickListener() {
-
+//	턴종료 버튼을 클릭하면 타이머의 작동 스위치의 상태를 바꿔주어서 시간이 흐르고 안흐르게 하려고 했다.
 			@Override
 			public void onClick(Component component) {
-				whiteTimer.getTimer().timerOperateSwitch();
-				blackTimer.getTimer().timerOperateSwitch();
+				((TimerPanel)getComponent(0)).getTimer().timerOperateSwitch();
+				((TimerPanel)getComponent(1)).getTimer().timerOperateSwitch();
 			}
 			
 		});
