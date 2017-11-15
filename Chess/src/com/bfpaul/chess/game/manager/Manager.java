578dafddf87347a935d44efb15e98c77c99348e7
김형수 <@DESKTOP-ATK5IMT>
2017-11-15 14:31:40 +0900

    ChessmanCreator, ChessmanSetter 추가 및 ChessmanType, Manager class 변경
    ChessmanType - type에 맞는 체스말을 생성하던 메서드 삭제, 체스말의 타입을 관리하는 클래스의 역할 범위에서
    벗어난다고 판단해서 삭제
    ChessmanCreator - 체스말의 생성을 전담하는 클래스를 chessman 패키지 내에 생성
    ChessmanSetter - 체스말을 보드위에 셋팅하는 역할을 전담하는 클래스로써 생성 시 초기의 체스말을 보드위에 셋팅해주며
    추후 다른 체스말들을 셋팅해주는 역할도 추가하기 위해서 Setter로써 역할을 하도록 할 계획이다.
    * Creator와 Setter를 분리하는 것에 대해서 생각이 많았는데 하나로 합쳐서 역할을 2개 부여하는 것 보다 하나의 역할을
    부여하는 것이 클래스가 정체성을 갖는데 좀더 옳다고 생각했다.
    Manager - 기존에 작성되어있던 메서드들을 다 삭제하고 ChessmanSetter를 생성하는 것으로써 체스말을 보드에 올려
    줄 수있도록 변경하였다.

--- Chess/src/com/bfpaul/chess/game/manager/Manager.java
+++ Chess/src/com/bfpaul/chess/game/manager/Manager.java
@@ -1,93 +1,13 @@
 package com.bfpaul.chess.game.manager;
 
 import com.bfpaul.chess.board.BoardView;
-import com.bfpaul.chess.chessman.ChessmanType;
+import com.bfpaul.chess.chessman.ChessmanSetter;
 
 public class Manager {
 
 	public BoardView board = new BoardView();
 
 	public Manager() {
-		createInitChessSet(true);
-		createInitChessSet(false);
-	}
-
-	private void createInitChessSet(boolean isWhite) {
-		ChessmanType[] chessmanType = ChessmanType.values();
-		for (ChessmanType type : chessmanType) {
-			switch(type) {
-			case PAWN : createInitPawnOnBoard(type, isWhite);
-			break;
-			case ROOK : createInitRookOnBoard(type, isWhite);
-			break;
-			case KNIGHT : createInitKnightOnBoard(type, isWhite);
-			break;
-			case BISHOP : createInitBishopOnBoard(type, isWhite);
-			break;
-			case QUEEN : createInitQueenOnBoard(type, isWhite);
-			break;
-			case KING : createInitKingOnBoard(type, isWhite);
-			break;
-			default : createInitPawnOnBoard(type, isWhite);
-			break;
-			}
-		}
-	}
-
-	private void createInitPawnOnBoard(ChessmanType type, boolean isWhite) {
-		int pawnInitCount = type.getInitCount();
-		for (int counter = 0; counter < pawnInitCount; counter++) {
-			if (isWhite) {
-				board.setChessmanOnSquare(type.createChessman(isWhite, type), counter, 1);
-			} else {
-				board.setChessmanOnSquare(type.createChessman(isWhite, type), counter, 6);
-			}
-		}
-	}
-	
-	private void createInitRookOnBoard(ChessmanType type, boolean isWhite) {
-		if(isWhite) {
-			board.setChessmanOnSquare(type.createChessman(isWhite, type), 0, 0);
-			board.setChessmanOnSquare(type.createChessman(isWhite, type), 7, 0);
-		} else {
-			board.setChessmanOnSquare(type.createChessman(isWhite, type), 0, 7);
-			board.setChessmanOnSquare(type.createChessman(isWhite, type), 7, 7);
-		}
-	}
-	
-	private void createInitKnightOnBoard(ChessmanType type, boolean isWhite) {
-		if(isWhite) {
-			board.setChessmanOnSquare(type.createChessman(isWhite, type), 1, 0);
-			board.setChessmanOnSquare(type.createChessman(isWhite, type), 6, 0);
-		} else {
-			board.setChessmanOnSquare(type.createChessman(isWhite, type), 1, 7);
-			board.setChessmanOnSquare(type.createChessman(isWhite, type), 6, 7);
-		}
-	}
-	
-	private void createInitBishopOnBoard(ChessmanType type, boolean isWhite) {
-		if(isWhite) {
-			board.setChessmanOnSquare(type.createChessman(isWhite, type), 2, 0);
-			board.setChessmanOnSquare(type.createChessman(isWhite, type), 5, 0);
-		} else {
-			board.setChessmanOnSquare(type.createChessman(isWhite, type), 2, 7);
-			board.setChessmanOnSquare(type.createChessman(isWhite, type), 5, 7);
-		}
-	}
-	
-	private void createInitQueenOnBoard(ChessmanType type, boolean isWhite) {
-		if(isWhite) {
-			board.setChessmanOnSquare(type.createChessman(isWhite, type), 4, 0);
-		} else {
-			board.setChessmanOnSquare(type.createChessman(isWhite, type), 4, 7);
-		}
-	}
-	
-	private void createInitKingOnBoard(ChessmanType type, boolean isWhite) {
-		if(isWhite) {
-			board.setChessmanOnSquare(type.createChessman(isWhite, type), 3, 0);
-		} else {
-			board.setChessmanOnSquare(type.createChessman(isWhite, type), 3, 7);
-		}
+		new ChessmanSetter(board);
 	}
 }
