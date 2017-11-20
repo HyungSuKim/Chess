f5a06c5ee3695844e2f468ea60b39bea564bc403
김형수 <@DESKTOP-ATK5IMT>
2017-11-20 14:59:05 +0900

    Queen의 특성을 가진 체스말로써 역할을 하기위해서 만들어졌다.
    따라서 Chessman을 구현하며 Queen을 체스말로써 표현하기위한 이미지와
    이동을 위한 정보를 제공한다.

---
+++ Chess/src/com/bfpaul/renewal/chess/chessman/Queen.java
@@ -0,0 +1,38 @@
+package com.bfpaul.renewal.chess.chessman;
+
+import java.awt.Image;
+/*
+ * Queen의 특성을 가진 체스말로써의 역할을 하기위해서 만들어졌다.   
+ * Queen의 표현을 위한 이미지와 이동을 위한 정보를 제공한다.
+ * 
+ * moveableSquareCount() { return -1 } : Queen의 움직임은 칸수의 제한이 없음을 표현하고자 했다.
+ */
+class Queen implements Chessman {
+	
+	private final boolean IS_WHITE;
+	
+	Queen(boolean isWhite) {
+		IS_WHITE = isWhite;
+	}
+
+	@Override
+	public Direction[] getDirection() {
+		return Direction.values();
+	}
+
+	@Override
+	public boolean isWhite() {
+		return IS_WHITE;
+	}
+
+	@Override
+	public int moveableSquareCount() {
+		return -1;
+	}
+
+	@Override
+	public Image getChessmanImage() {
+		return ChessmanImage.getChessmanImage(IS_WHITE, ChessmanType.QUEEN);
+	}
+	
+}
