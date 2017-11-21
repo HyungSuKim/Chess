bd60d18a6fceb5e5091a182e842223f7f3fcb875
김형수 <@DESKTOP-ATK5IMT>
2017-11-21 16:32:21 +0900

    ChessmanController class 생성
    체스말이 체스판 위에서 표현해야 될 3가지 항목(놓여지는 것, 사라지는 것, 이동하는 것)을 위해 존재하는 클래스.
    ChessmanController는 체스말이 제공하는 데이터를 가공하여 보드에 제공함으로써 체스말이 보드위에서 표현해야 할 3가지
    항목을 표현할 수 있도록 해 줄 계획이다.

---
+++ Chess/src/com/bfpaul/renewal/chess/board/ChessmanController.java
@@ -0,0 +1,23 @@
+package com.bfpaul.renewal.chess.board;
+
+import com.bfpaul.renewal.chess.chessman.Chessman;
+
+/* 
+ * 체스말들을 이용해서 보드위에 놓여지는 것과 사라지는 것, 이동하는 것을 보드위에 표현하기 위해 존재한다. 
+ * ChessmanController는 칸 위에 있는 말의 이동가능한 경로를 계산하여 보드에 제공함으로써 체스말이 이동 할 수있는 경로를 표현 할 수 있게 해준다. 
+ * 
+ * 작성중...
+ */
+class ChessmanController {
+	
+	private ChessmanController() { }
+	
+	public static void pickChessman(Chessman chessman) {
+
+			// 체스말의 움직임 가능 경로를 반환하는 메서드 호출
+		
+	}
+	
+	
+	 
+}
