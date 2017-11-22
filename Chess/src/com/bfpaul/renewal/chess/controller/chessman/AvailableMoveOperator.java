8270162f9b0482ca4454f14dcddc68a0b644e9c0
김형수 <@DESKTOP-ATK5IMT>
2017-11-22 23:20:40 +0900

    moveableSquareCount 사용 고민...
    움직일수있는 칸수를 for문으로 반영하려고 하니 뭔가 코드가 난잡해졌다...
    처음부터 다시...

--- Chess/src/com/bfpaul/renewal/chess/controller/chessman/AvailableMoveOperator.java
+++ Chess/src/com/bfpaul/renewal/chess/controller/chessman/AvailableMoveOperator.java
@@ -11,91 +11,98 @@ class AvailableMoveOperator {
 	}
 
 	public static ArrayList<Coordinate> availableMoveCoordinate(Chessman chessman, int x, int y) {
-		System.out.println(chessman.getChessmanType());
 		ArrayList<Coordinate> availMove = new ArrayList<>();
-		for (Direction direction : chessman.getDirection()) {
-			System.out.println(direction);
-			switch (direction) {
-			case UP:
-				availMove.add(availableMoveUp(chessman, x, y));
-				break;
-			case DOWN:
-				availMove.add(availableMoveDown(chessman, x, y));
-				break;
-			case LEFT:
-				availMove.add(availableMoveLeft(chessman, x, y));
-				break;
-			case RIGHT:
-				availMove.add(availableMoveRight(chessman, x, y));
-				break;
-			case UP_LEFT:
-				availMove.add(availableMoveUpLeft(chessman, x, y));
-				break;
-			case UP_RIGHT:
-				availMove.add(availableMoveUpRight(chessman, x, y));
-				break;
-			case DOWN_LEFT:
-				availMove.add(availableMoveDownLeft(chessman, x, y));
-				break;
-			case DOWN_RIGHT:
-				availMove.add(availableMoveDownRight(chessman, x, y));
-				break;
-			default:
-				break;
+		int moveableSquareCount = chessman.getMoveableSquareCount();
+		
+		if(moveableSquareCount == -1) {
+			// ¹«ÇÑ´ë
+		} else {
+			for(int count = 0; count < moveableSquareCount; count++) {
+				for (Direction direction : chessman.getDirection()) {
+					System.out.println(direction);
+					switch (direction) {
+					case UP:
+						availMove.add(availableMoveUp(chessman, x, y, count));
+						break;
+					case DOWN:
+						availMove.add(availableMoveDown(chessman, x, y, count));
+						break;
+					case LEFT:
+						availMove.add(availableMoveLeft(chessman, x, y, count));
+						break;
+					case RIGHT:
+						availMove.add(availableMoveRight(chessman, x, y, count));
+						break;
+					case UP_LEFT:
+						availMove.add(availableMoveUpLeft(chessman, x, y, count));
+						break;
+					case UP_RIGHT:
+						availMove.add(availableMoveUpRight(chessman, x, y, count));
+						break;
+					case DOWN_LEFT:
+						availMove.add(availableMoveDownLeft(chessman, x, y, count));
+						break;
+					case DOWN_RIGHT:
+						availMove.add(availableMoveDownRight(chessman, x, y, count));
+						break;
+					default:
+						break;
+					}
+				}
 			}
 		}
 		return availMove;
 	}
 
-	static Coordinate availableMoveUp(Chessman chessman, int x, int y) {
-		if (Coordinate.isValidate(x, (y + chessman.getMoveableSquareCount()))) {
-			return new Coordinate(x, (y + chessman.getMoveableSquareCount()));
+	static Coordinate availableMoveUp(Chessman chessman, int x, int y, int count) {
+		if (Coordinate.isValidate(x, (y + count))) {
+			return new Coordinate(x, (y + count));
 		} else {
 			return new Coordinate(x, y);
 		}
 	}
 
-	static Coordinate availableMoveDown(Chessman chessman, int x, int y) {
-		if (Coordinate.isValidate(x, (y - chessman.getMoveableSquareCount()))) {
-			return new Coordinate(x, (y - chessman.getMoveableSquareCount()));
+	static Coordinate availableMoveDown(Chessman chessman, int x, int y, int count) {
+		if (Coordinate.isValidate(x, (y - count))) {
+			return new Coordinate(x, (y - count));
 		} else {
 			return new Coordinate(x, y);
 		}
 	}
 
-	static Coordinate availableMoveRight(Chessman chessman, int x, int y) {
-		if (Coordinate.isValidate((x + chessman.getMoveableSquareCount()), y)) {
-			return new Coordinate((x + chessman.getMoveableSquareCount()), y);
+	static Coordinate availableMoveRight(Chessman chessman, int x, int y, int count) {
+		if (Coordinate.isValidate((x + count), y)) {
+			return new Coordinate((x + count), y);
 		} else {
 			return new Coordinate(x, y);
 		}
 	}
 
-	static Coordinate availableMoveLeft(Chessman chessman, int x, int y) {
-		if (Coordinate.isValidate((x - chessman.getMoveableSquareCount()), y)) {
-			return new Coordinate((x - chessman.getMoveableSquareCount()), y);
+	static Coordinate availableMoveLeft(Chessman chessman, int x, int y, int count) {
+		if (Coordinate.isValidate((x - count), y)) {
+			return new Coordinate((x - count), y);
 		} else {
 			return new Coordinate(x, y);
 		}
 	}
 
-	static Coordinate availableMoveUpLeft(Chessman chessman, int x, int y) {
-		if (Coordinate.isValidate((x - chessman.getMoveableSquareCount()), (y + chessman.getMoveableSquareCount()))) {
-			return new Coordinate((x - chessman.getMoveableSquareCount()), (y + chessman.getMoveableSquareCount()));
+	static Coordinate availableMoveUpLeft(Chessman chessman, int x, int y, int count) {
+		if (Coordinate.isValidate((x - count), (y + count))) {
+			return new Coordinate((x - count), (y + count));
 		} else {
 			return new Coordinate(x, y);
 		}
 	}
 
-	static Coordinate availableMoveUpRight(Chessman chessman, int x, int y) {
-		if (Coordinate.isValidate((x + chessman.getMoveableSquareCount()), (y + chessman.getMoveableSquareCount()))) {
-			return new Coordinate((x + chessman.getMoveableSquareCount()), (y + chessman.getMoveableSquareCount()));
+	static Coordinate availableMoveUpRight(Chessman chessman, int x, int y, int count) {
+		if (Coordinate.isValidate((x + count), (y + count))) {
+			return new Coordinate((x + count), (y + count));
 		} else {
 			return new Coordinate(x, y);
 		}
 	}
 
-	static Coordinate availableMoveDownLeft(Chessman chessman, int x, int y) {
+	static Coordinate availableMoveDownLeft(Chessman chessman, int x, int y, int count) {
 		System.out.println(Coordinate.isValidate((x - chessman.getMoveableSquareCount()), (y - chessman.getMoveableSquareCount())));
 		if (Coordinate.isValidate((x - chessman.getMoveableSquareCount()), (y - chessman.getMoveableSquareCount()))) {
 			return new Coordinate((x - chessman.getMoveableSquareCount()), (y - chessman.getMoveableSquareCount()));
@@ -104,41 +111,11 @@ class AvailableMoveOperator {
 		}
 	}
 
-	static Coordinate availableMoveDownRight(Chessman chessman, int x, int y) {
+	static Coordinate availableMoveDownRight(Chessman chessman, int x, int y, int count) {
 		if (Coordinate.isValidate((x + chessman.getMoveableSquareCount()), (y - chessman.getMoveableSquareCount()))) {
 			return new Coordinate((x + chessman.getMoveableSquareCount()), (y - chessman.getMoveableSquareCount()));
 		} else {
 			return new Coordinate(x, y);
 		}
 	}
-
-	// public static ArrayList<Coordinate> kingAvailableMoveCoordinate(Chessman
-	// chessman, int x, int y) {
-	//
-	// }
-	//
-	// public static ArrayList<Coordinate> queenAvailableMoveCoordinate(Chessman
-	// chessman, int x, int y) {
-	//
-	// }
-	//
-	// public static ArrayList<Coordinate> bishopAvailableMoveCoordinate(Chessman
-	// chessman, int x, int y) {
-	//
-	// }
-	//
-	// public static ArrayList<Coordinate> knightAvailableMoveCoordinate(Chessman
-	// chessman, int x, int y) {
-	//
-	// }
-	//
-	// public static ArrayList<Coordinate> rookAvailableMoveCoordinate(Chessman
-	// chessman, int x, int y) {
-	//
-	// }
-
-	public static ArrayList<Coordinate> pawnAvailableMoveCoordinate(Chessman chessman, int x, int y) {
-		return pawnAvailableMoveCoordinate(chessman, x, y);
-	}
-
 }
