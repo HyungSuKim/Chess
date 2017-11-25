f2ab1d5e1ee2bd382549ea520278d9f226a7b196
김형수 <@DESKTOP-ATK5IMT>
2017-11-25 11:31:25 +0900

    Knight의 이동 개념 오류에 따른 주석 및 메서드 수정
    Knight의 이동에 대해서 주석으로 작성해 놓은 개념이 잘 못 되었음을 인지 하고 수정하였다. 따라서 메서드
    getDirection()의 내용 또한 수정하였다.

--- Chess/src/com/bfpaul/renewal/chess/chessman/Knight.java
+++ Chess/src/com/bfpaul/renewal/chess/chessman/Knight.java
@@ -7,6 +7,14 @@ import java.awt.Image;
  * 
  * Direction[] getDirection() { return Direction.values(); }
  * -> Knight의 이동은 UP, DOWN, RIGHT, LEFT로 2칸 이동 후 UP_RIGHT, UP_LEFT, DOWN_RIGHT, DOWN_LEFT로 1칸 이동한다고 생각하였다.
+ * 
+ *  * 	public Direction[] getDirection() {	return new Direction[] { Direction.UP, Direction.DOWN, Direction.RIGHT, Direction.LEFT }; }
+ * 수정사항 경우 1 -> Knight의 이동은 UP, DOWN, RIGHT, LEFT로 2칸 이동후
+ * 				UP/DOWN의 경우 LEFT나 RIGHT로 LEFT/RIGHT의 경우 UP이나 DOWN으로 1칸 이동한다
+ * 				<위키백과의 설명은 경우 1과 동일하게 설명하고있다 2칸 직진 후 좌우 1칸이동>
+ * 수정사항 경우 2 -> Knight의 이동은 UP, DOWN, RIGHT, LEFT로 1칸 이동후
+ * 				UP의 경우 UP_LEFT, UP_RIGHT로 / DOWN의 경우 DOWN_LEFT, DOWN_RIGHT로
+ * 				LEFT의 경우 UP_LEFT, DOWN_LEFT로 / RIGHT의 경우 UP_RIGHT로 DOWN_RIGHT로 1칸 이동한다.
  */
 public class Knight implements Chessman {
 	
@@ -18,7 +26,7 @@ public class Knight implements Chessman {
 
 	@Override
 	public Direction[] getDirection() {
-		return Direction.values();
+		return new Direction[] { Direction.UP, Direction.DOWN, Direction.RIGHT, Direction.LEFT };
 	}
 
 	@Override
